3a758aac2437df046d2cb4e9632982c5c0ca40a9
asd2599 <asd2590@naver.com>
2026-02-26 17:50:59 +0900

    feat: Initial project setup with React frontend, Express backend, PostgreSQL database, and user authentication features.

---
+++ back/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is Running!");
+});
+
+const userRoutes = require("./routes/userRoutes");
+app.use(userRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 서버가 포트 ${PORT}에서 작동 중입니다.`);
+});
